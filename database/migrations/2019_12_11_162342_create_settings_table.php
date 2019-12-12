85eb992da23a9e954218bf81c774162cc3a73a0e
sahapranta <pranta1204@gmail.com>
2019-12-12 11:22:30 +0600

    setings

--- database/migrations/2019_12_11_162342_create_settings_table.php
+++ database/migrations/2019_12_11_162342_create_settings_table.php
@@ -15,6 +15,7 @@ class CreateSettingsTable extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
             $table->json('data');
             $table->timestamps();
         });
